b5d8e25f174952e7c088373e905d9a0c35a46ba8
Julyana Mira <julyanamira@gmail.com>
2026-06-22 17:17:30 -0300

    ✨ feat: Adicionando o DTO e o BO com lógica de registro e meta de consumo de água

---
+++ src/main/java/com/ninju/bo/WaterLogBO.java
@@ -0,0 +1,69 @@
+package com.ninju.bo;
+
+import com.ninju.dao.WaterLogDao;
+import com.ninju.dao.AuditLogDao;
+import com.ninju.dao.UserDao;
+import com.ninju.dto.WaterLogDTO;
+import com.ninju.model.WaterLog;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.time.LocalDate;
+
+@ApplicationScoped
+public class WaterLogBO {
+    
+    @Inject WaterLogDao waterLogDao;
+    @Inject UserDao userDao;
+    @Inject AuditLogDao auditLogDao;
+
+    public WaterLogDTO getTodayLog(Long userId, LocalDate date, String executedBy) {
+        auditLogDao.save("HIDRATACAO_DIA: " + date, executedBy);
+
+        WaterLog log = waterLogDao.findByUserAndDate(userId, date);
+
+        if (log == null) {
+            return null;
+        }
+
+        return new WaterLogDTO(log.getId(), log.getDate(), log.getAmountMl(), log.getGoalMl(),
+        (double) log.getAmountMl() / log.getGoalMl() * 100);
+    }
+
+    public WaterLogDTO addWater(Long userId, LocalDate date, int amountMl, String executedBy) {
+        WaterLog log = waterLogDao.findByUserAndDate(userId, date);
+
+        if(log == null){
+            log = new WaterLog();
+            log.setUser(userDao.findById(userId));
+            log.setDate(date);
+            log.setAmountMl(amountMl);
+            log.setGoalMl(2000); // Default goal
+            waterLogDao.save(log);
+
+        }else{
+            log.setAmountMl((log.getAmountMl() + amountMl));
+            waterLogDao.save(log);
+        }
+
+        return getTodayLog(userId, date, executedBy);
+    }
+
+    public WaterLogDTO updateGoal(Long userId, LocalDate date, int goalMl, String executedBy) {
+       WaterLog log = waterLogDao.findByUserAndDate(userId, date);
+
+        if (log == null) {
+            log = new WaterLog();
+            log.setUser(userDao.findById(userId));
+            log.setDate(date);
+            log.setAmountMl(0);
+        }
+
+        log.setGoalMl(goalMl); //atualiza só a meta
+        waterLogDao.save(log);
+
+
+        return getTodayLog(userId, date, executedBy);
+    }
+}
---
+++ src/main/java/com/ninju/dto/WaterLogDTO.java
@@ -0,0 +1,20 @@
+package com.ninju.dto;
+
+import java.time.LocalDate;
+
+public class WaterLogDTO {
+    public Long id;
+    public LocalDate date;
+    public Integer amountMl;
+    public int goalMl;
+    public double percentageAchieved;
+
+    public WaterLogDTO(Long id, LocalDate date, Integer amountMl, int goalMl, double percentageAchieved) {
+        this.id = id;
+        this.date = date;
+        this.amountMl = amountMl;
+        this.goalMl = goalMl;
+        this.percentageAchieved = (amountMl / goalMl) * 100;
+    }
+    
+}
