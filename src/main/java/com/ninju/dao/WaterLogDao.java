e1327fefa035c78da71fd2e9550d488a7e1c0ce7
Julyana Mira <julyanamira@gmail.com>
2026-06-22 16:18:40 -0300

    ✨ feat: Adicionando DAO para consulta e persistência de WaterLog

---
+++ src/main/java/com/ninju/dao/WaterLogDao.java
@@ -0,0 +1,32 @@
+package com.ninju.dao;
+
+
+import com.ninju.model.WaterLog;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import java.time.LocalDate;
+
+@ApplicationScoped
+public class WaterLogDao {
+    
+    @PersistenceContext
+    EntityManager em;
+
+    @Transactional
+    public void save(WaterLog waterLog) {
+        em.persist(waterLog);
+    }
+
+    @Transactional
+    public WaterLog findByUserAndDate(Long userId, LocalDate date) {
+    return em.createQuery(
+        "SELECT w FROM WaterLog w WHERE w.user.id = :uid AND w.date = :date", WaterLog.class)
+        .setParameter("uid", userId)
+        .setParameter("date", date)
+        .getResultStream()
+        .findFirst()
+        .orElse(null);
+    }
+}
